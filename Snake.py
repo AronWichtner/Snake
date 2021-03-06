45cdf34f4b95e19264e149d15f8e11c9fb7381a4
AronWichtner <aronwichtner@gmail.com>
2021-03-06 19:04:26 +0100

    first commit

---
+++ Snake.py
@@ -0,0 +1,10 @@
+class Snake:
+    def __init__(self):
+        self.body = [[10, 10], [10, 11], [10, 12]]
+        self.move = [0, 0]
+
+    def updateSnake(self):
+        newBody = self.body[:-1]
+        newHead = [self.body[0][0] + self.move[0], self.body[0][1] + self.move[1]]
+        newBody.insert(0, newHead)
+        self.body = newBody
