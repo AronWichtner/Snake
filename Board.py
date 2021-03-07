1159ced49ffbef30ebe1ef172007c17fe9d55bfc
AronWichtner <aronwichtner@gmail.com>
2021-03-07 22:24:13 +0100

    add init def

--- Board.py
+++ Board.py
@@ -1,3 +1,4 @@
 class Board:
-    cell_size = 40  # pixel size
-    cell_number = 20  # amount of cells
+    def __init__(self):
+        self.cell_size = 40  # pixel size
+        self.cell_number = 20  # amount of cells
