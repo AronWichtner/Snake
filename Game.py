21be35e9696e1b32a3dc9ec42c539df9b0f63a06
AronWichtner <aronwichtner@gmail.com>
2021-03-14 00:09:11 +0100

    restructure drawObjects method

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 from Snake import *
 from Food import *
+from Poison import *
 from Board import *
 import pygame, time, sys
 
@@ -9,42 +10,71 @@ GRASSGREEN = (160, 231, 93)
 APPLERED = (199, 0, 57)
 SNAKEGREEN = (51, 156, 70)
 
-
 class Game:
     def __init__(self):
         self.snake = Snake()
         self.food = Food()
+        self.poison = Poison()
         self.board = Board()
         self.screen = pygame.display.set_mode((self.board.cell_size * self.board.cell_number,
                                                self.board.cell_size * self.board.cell_number))
 
-    def drawObjects(self):
-        while True:
-            food_on_snake = False
-            for element in self.snake.body:
-                if self.food.position == element:
-                    food_on_snake = True
-                    break
-                else:
-                    continue
-            if food_on_snake:
-                self.food = Food()
-                continue
-            else:
-                food_rect = pygame.Rect(self.food.position[0] * self.board.cell_size,
-                                        self.food.position[1] * self.board.cell_size,
-                                        self.board.cell_size, self.board.cell_size)
-                pygame.draw.rect(self.screen, APPLERED, food_rect)
+    def checkForSamePositions(self):
+        food_on_snake = False
+        poison_on_snake = False
+        poison_on_food = False
+        for element in self.snake.body:
+            if self.food.position == element:
+                food_on_snake = True
                 break
+            elif self.poison.position == element:
+                poison_on_snake = True
+                break
+            elif self.poison.position == self.food.position:
+                poison_on_food = True
+                break
+            else:
+                continue
+        return [food_on_snake, poison_on_snake, poison_on_food]
 
+    def drawSnake(self):
         for part in self.snake.body:
             snake_body = pygame.Rect(part[0] * self.board.cell_size,
                                      part[1] * self.board.cell_size,
                                      self.board.cell_size, self.board.cell_size)
             pygame.draw.rect(self.screen, SNAKEGREEN, snake_body)
 
+    def drawFood(self):
+        food_rect = pygame.Rect(self.food.position[0] * self.board.cell_size,
+                                self.food.position[1] * self.board.cell_size,
+                                self.board.cell_size, self.board.cell_size)
+        self.screen.blit(self.food.graphics, food_rect)
+
+    def drawPoison(self):
+        poison_rect = pygame.Rect(self.poison.position[0] * self.board.cell_size,
+                                  self.poison.position[1] * self.board.cell_size,
+                                  self.board.cell_size, self.board.cell_size)
+        self.screen.blit(self.poison.graphics, poison_rect)
+
+    def drawObjects(self):
+        while True:
+            positions = self.checkForSamePositions()
+            if positions[0]:
+                self.food = Food()
+                continue
+            elif positions[1]:
+                self.poison = Poison()
+                continue
+            elif positions[2]:
+                self.poison = Poison()
+            else:
+                self.drawFood()
+                self.drawPoison()
+                break
+        self.drawSnake()
+
     def checkForGameOver(self):
-        # snake hit itself
+        # check if snake ate poison
         if self.checkIfSnakeeatsItslef() or self.movingFromMap():
             return True
         else:
@@ -76,6 +106,7 @@ class Game:
         # print big Game Over sign
         # new game or end (button?)
         time.sleep(3)
+        pygame.quit()
         sys.exit()
 
     def checkForCollision(self):
