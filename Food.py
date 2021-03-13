825ebbd1d6d36a5c0cc2716cbf88bd949562e771
AronWichtner <aronwichtner@gmail.com>
2021-03-14 00:06:01 +0100

    load apple from Graphics

--- Food.py
+++ Food.py
@@ -1,11 +1,16 @@
 import random
+import os
+import pygame
+
 
 class Food:
-    def __init__(self):
-        self.x = self.generateRandomCoorinate()
-        self.y = self.generateRandomCoorinate()
-        self.position = [self.x, self.y]
 
-    def generateRandomCoorinate(self):
+    def __init__(self):
+        self.x = self.generateRandomCoordinate()
+        self.y = self.generateRandomCoordinate()
+        self.position = [self.x, self.y]
+        self.graphics = pygame.image.load(os.path.join('Graphics/apple.png'))
+
+    def generateRandomCoordinate(self):
         x = random.randrange(0, 19)
         return x
